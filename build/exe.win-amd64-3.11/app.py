0419ef26ac3e6fa9fa531261b2b08e9aa00b99fb
Paul Lam <paullam97@gmail.com>
2026-06-12 18:13:37 +0800

    Upload build

---
+++ build/exe.win-amd64-3.11/app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:66b5466ee344f701456dd7bed028ba9377c01636c496ab023915f9223d990623
+size 4228
---
+++ build/exe.win-amd64-3.11/transcribe.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8008a95ac44fc7e46bdbc8199f07f170a75c2eda942c9f388434c0abb03d09b3
+size 7180
